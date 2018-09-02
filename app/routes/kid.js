04be8a6b27c28b885e14fc73804a0140dbe099ed
Felipe Dias <zielinski.felipe@gmail.com>
2018-09-02 00:24:41 -0300

    Add kids route and controllers

--- app/routes/kid.js
+++ app/routes/kid.js
@@ -2,17 +2,17 @@ module.exports = function(app) {
     var kidController = require('../controllers/kid.js');
 
     // Create a new kid
-    //app.post('/kid', kidController.create);
+    app.post('/kid', kidController.create);
 
     // Retrieve all kids
-    //app.get('/kids', kidController.findAll);
+    app.get('/kids', kidController.findAll);
 
     // Retrive a single kid with kid id
-    //app.get('/kid/:kidId', kidController.findOne);
+    app.get('/kid/:kidId', kidController.findOne);
 
     // Update an kid with kid id
-    //app.put('/kid/:kidId', kidController.update);
+    app.put('/kid/:kidId', kidController.update);
 
     // Delete an kid with kid id
-    //app.delete('/kids/:kidId', kidController.delete);
+    // app.delete('/kids/:kidId', kidController.delete);
 }
