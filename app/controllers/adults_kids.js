3432e1284e6f6c25c4b8b78d28757cf6a163780f
Fábio Almeida <fabio.h.s.almeida@gmail.com>
2018-09-02 12:07:29 -0300

    Incluindo nome da criança ao listar crianças vinculadas ao adulto

--- app/controllers/adults_kids.js
+++ app/controllers/adults_kids.js
@@ -2,7 +2,7 @@ const db = require('../models/index');
 
 exports.findKidsByAdultId = async function(req, res) {
     const kids = await db.sequelize.query(
-        'SELECT id, kid_id, adult_id, created_at, updated_at FROM adults_kids WHERE adult_id=:adultId',
+        'SELECT ak.id, ak.kid_id, k.name as kid_name, ak.adult_id, ak.created_at, ak.updated_at FROM adults_kids ak, kids k WHERE ak.adult_id=:adultId AND ak.kid_id=k.id',
         { replacements: { 
             adultId: req.params.adultId
         }, type: db.sequelize.QueryTypes.SELECT
